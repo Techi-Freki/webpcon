fed8df0e0b7ba7efa512233379a7d13338738954
Techi-Freki <techifreki@proton.me>
2026-03-25 11:37:36 -0400

    fixing method sig

--- webpcon/converter.py
+++ webpcon/converter.py
@@ -10,7 +10,7 @@ class Format(Enum):
 
 class Converter(object):
     @staticmethod
-    def convert(input_file: str, output_file: str, file_format = Format.PNG: Format):
+    def convert(input_file:str, output_file:str, file_format:Format=Format.PNG):
         if not os.path.exists(input_file):
             raise FileNotFoundError(f"{input_file} does not exist.")
 
@@ -22,5 +22,5 @@ class Converter(object):
 
                 img.save(output_file, file_format.name, quality=85)
                 print(f"File successfully converted, file saved to {output_file}.")
-        except (IOError) as e:
+        except IOError as e:
             print(f"Error converting file: {e}")
